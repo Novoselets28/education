6fa1714cdb10f859d13decfdc62f4012b7913f0c
Artem Novoselets <artem28novoselets11@gmail.com>
2023-10-04 00:22:38 +0300

    React Grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import {
+  Grid, PagingPanel, SearchPanel, Table, TableHeaderRow, TableRowDetail, Toolbar
+} from '@devexpress/dx-react-grid-material-ui'
+import { IntegratedFiltering, IntegratedPaging, PagingState, RowDetailState, SearchState } from '@devexpress/dx-react-grid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const App = () => {
+  
+  const [films, setFilms] = useState([]);
+  const [columns] = useState([
+    { name: 'id', title: 'ID' },
+    { name: 'title', title: 'Title' },
+    {name: 'vote_count', title:'vote_count'}
+  ]);
+
+  const RowDetail = ({ row }) => (
+    <div>
+      {<img
+        src={`https://image.tmdb.org/t/p/w500${row.poster_path}`}
+        alt={row.title}
+        style={{ width: '100px', height: 'auto' }}
+      />}
+      {' '}
+      {row.title}
+      {' '}
     </div>
   );
-}
 
+  useEffect(()=>{
+    fetch('https://api.themoviedb.org/3/trending/all/day?api_key=3d9706b6cce789c4f8aaf7a09aec0943')
+  .then(response => response.json())
+  .then(data => setFilms(data.results))
+  .catch(err => console.error(err));
+  })
+
+  return (
+    <div>
+      <Grid
+        rows={films}
+        columns={columns}>
+        <RowDetailState
+          defaultExpandedRowIds={[2, 5]}/>
+        <SearchState defaultValue="" />
+        <IntegratedFiltering />
+        <PagingState
+          defaultCurrentPage={0}
+          pageSize={5}
+        />
+        <IntegratedPaging />
+        <Table />
+        <TableHeaderRow />
+        <TableRowDetail
+          contentComponent={RowDetail}/>
+        <Toolbar />
+        <SearchPanel />
+        <PagingPanel />
+      </Grid>
+    </div>
+  )
+}
 export default App;
