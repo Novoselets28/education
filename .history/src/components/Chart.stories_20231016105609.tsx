92c53f2a2a93a03403f6fbece1a1b90ff89a6dea
Artem Novoselets <artem28novoselets11@gmail.com>
2023-10-16 12:23:59 +0300

    add story for Chart page

---
+++ .history/src/components/Chart.stories_20231016105609.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+
+import { MockedProvider } from '@apollo/client/testing';
+
+import { GET_CHARACTERS } from '../apollo/people';
+
+import client from '../apollo/client';
+
+import Chart from './Chart';
+
+export default {
+  title: 'Chart',
+  component: Chart,
+  decorators: [
+    (Story) => (
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: GET_CHARACTERS
+            },
+            result: {
+              data: {
+                characters: {
+                  results: [
+                    {
+                      name: 'Character 1',
+                      created: '2017-01-01T00:00:00.000Z',
+                      gender: 'Male'
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        ]}
+      >
+        <Story />
+      </MockedProvider>
+    )
+  ]
+};
+
+export const Default = () => (
+  <ApolloProvider client={client}>
+    <Chart />
+  </ApolloProvider>
+);
